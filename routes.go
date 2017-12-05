451b45ca6651dfc4eb93ce0a8ed021f668643c77
albertkurnia <if413071@gmail.com>
2017-12-05 19:35:28 +0700

    add routes file

---
+++ routes.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net/http"
+)
+
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+type Routes []Route
+
+var routes = Routes{
+
+	Route{
+		"GetPosts",
+		"GET",
+		"/posts",
+		GetPosts,
+	},
+	Route{
+		"GetPost",
+		"GET",
+		"/post/{id}",
+		GetPost,
+	},
+	Route{
+		"CreatePost",
+		"POST",
+		"/post/{id}",
+		CreatePost,
+	},
+	Route{
+		"DeletePost",
+		"DELETE",
+		"/post/{id}",
+		CreatePost,
+	},
+}
